725a0f3fdced447b1532729b31339d40902d7105
rydufren-microsoft <66136035+rydufren-microsoft@users.noreply.github.com>
2020-07-07 19:02:30 -0400

    Updated shortcut generation. (#8)

--- GenerateShortcut.ps1
+++ GenerateShortcut.ps1
@@ -1,19 +1,73 @@
-$rootPath = Split-Path $script:MyInvocation.MyCommand.Path
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$rootPath\pChat.lnk")
-$Shortcut.TargetPath = "$PsHome\powershell.exe"
-$Shortcut.Arguments = "-windowstyle hidden $rootPath\src\LaunchRoomSelection.ps1"
-$Shortcut.IconLocation = "$rootPath\src\resources\pChatIcon.ico"
+$psFile = [System.IO.Path]::Combine($PsHome , "powershell.exe")
+
+$rootPath = Split-Path $script:MyInvocation.MyCommand.Path
+$srcPath = [System.IO.Path]::Combine($rootPath , "src")
+$resourcePath = [System.IO.Path]::Combine($srcPath , "resource")
+$icoFile = [System.IO.Path]::Combine($resourcePath , "pChatIcon.ico")
+$supportShortcutsPath = [System.IO.Path]::Combine($srcPath , "lnks")
+
+
+$table = @( @{Variable="psFile";   Value=$psFile},
+            @{Variable="rootPath";    Value=$rootPath},
+            @{Variable="srcPath";   Value=$srcPath},
+            @{Variable="resourcePath";   Value=$resourcePath},
+            @{Variable="icoFile";   Value=$icoFile},
+            @{Variable="supportShortcutsPath"; Value=$supportShortcutsPath} )
+
+$table | ForEach {[PSCustomObject]$_} | Format-Table -AutoSize
+
+if(!(Test-Path -path $supportShortcutsPath))  {  
+    New-Item -ItemType directory -Path $supportShortcutsPath
+    Write-Host "Folder path has been created successfully at: " $supportShortcutsPath    
+}
+else { 
+    Write-Host "The given folder path $supportShortcutsPath already exists"; 
+}
+
+$shortcutPath = [System.IO.Path]::Combine($rootPath , "pChat.lnk")
+$Shortcut = $WshShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $psFile
+$Shortcut.Arguments = "-windowstyle hidden " + [System.IO.Path]::Combine($srcPath, "LaunchRoomSelection.ps1")
+$Shortcut.IconLocation = $icoFile
 $Shortcut.Save()
 
-$Shortcut = $WshShell.CreateShortcut("$rootPath\src\lnks\LaunchRoomCreation.lnk")
-$Shortcut.TargetPath = "$PsHome\powershell.exe"
-$Shortcut.Arguments = "-windowstyle hidden $rootPath\src\LaunchRoomCreation.ps1"
-$Shortcut.IconLocation = "$rootPath\src\resources\pChatIcon.ico"
+if([System.IO.File]::Exists($shortcutPath)) {
+    Write-Host "`nCreation of main shortcut $shortcutPath was successful`n" -ForegroundColor green; 
+}
+else {
+    Write-Error "Failed to create the file $shortcutPath"
+}
+
+
+$shortcutPath = [System.IO.Path]::Combine($supportShortcutsPath , "LaunchRoomCreation.lnk")
+$Shortcut = $WshShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $psFile
+$Shortcut.Arguments = "-windowstyle hidden " + [System.IO.Path]::Combine($srcPath, "LaunchRoomCreation.ps1")
+$Shortcut.IconLocation = $icoFile
 $Shortcut.Save()
 
-$Shortcut = $WshShell.CreateShortcut("$rootPath\src\lnks\LaunchRoom.lnk")
-$Shortcut.TargetPath = "$PsHome\powershell.exe"
-$Shortcut.Arguments = "-windowstyle hidden $rootPath\src\LaunchRoom.ps1"
-$Shortcut.IconLocation = "$rootPath\src\resources\pChatIcon.ico"
+if([System.IO.File]::Exists($shortcutPath)) {
+    Write-Host "Creation of shortcut $shortcutPath was successful"-ForegroundColor green; 
+}
+else {
+    Write-Error "Failed to create the file $shortcutPath"
+}
+
+$shortcutPath = [System.IO.Path]::Combine($supportShortcutsPath , "LaunchRoom.lnk")
+$Shortcut = $WshShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $psFile
+$Shortcut.Arguments = "-windowstyle hidden " + [System.IO.Path]::Combine($srcPath, "LaunchRoom.ps1")
+$Shortcut.IconLocation = $icoFile
 $Shortcut.Save()
+
+if([System.IO.File]::Exists($shortcutPath)) {
+    Write-Host "Creation of shortcut $shortcutPath was successful"-ForegroundColor green; 
+}
+else {
+    Write-Error "Failed to create the file $shortcutPath"
+}
+
+Write-Host "`nDone"; 
+
+Read-Host -Prompt "`nPress Enter to exit"
